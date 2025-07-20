051b5c5ae87907eef3f6a99914ed301b22c728cf
Aryan Arshad <126335847+A4-AryanArshad@users.noreply.github.com>
2025-07-20 12:48:54 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS middleware - must be before any routes or express.json()
 app.use(cors({
-  origin: 'https://e-frontend-n5sm.vercel.app',
+  origin: 'https://e-frontend-jz2l.vercel.app',
    credentials: true
  }));
 
